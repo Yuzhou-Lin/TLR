7d885f722ca199a0e51d87a6b53c7ce02bb39fd7
Yuzhou Lin <yuzhoulin@dhcp-10-250-49-24.harvard.edu>
2025-09-17 17:06:52 -0400

    Update functions

--- R/JCCorrect.R
+++
@@ -1,9 +0,0 @@
-
-
-JCCorrect = function(pval){
-  z = stats::qnorm(pval,lower.tail = F)
-  res= nullParaEst(z)
-  pval.JC = stats::pnorm(z,mean = res$mu,sd = res$s,lower.tail = F)
-  return(pval.JC)
-}
-
--- R/Sobel.R
+++
@@ -1,8 +0,0 @@
-
-
-
-## Sobel
-Sobel = function(Z1,Z2){
-  T = (Z1*Z2)^2/(Z1^2+Z2^2)
-  pchisq(T,df=1,lower.tail = F)
-}
